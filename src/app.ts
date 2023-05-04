570fdcab3d91bf2554299daeabc8be83aeb454f2
Çağatay <durucagatay01@gmail.com>
2023-05-04 10:55:24 +0300

    Initialized Typescript

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express, { Application, Request, Response } from 'express'
+import path from 'path';
+
 
 const app = express()
+const router = express.Router()
 const port = 3000
 
+app.use(express.static(__dirname + 'public')); //Serves resources from public folder
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello NodeJS!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`NodeJS app listening on port ${port}`)
 })
